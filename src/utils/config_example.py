b4fcc7fa5a64a5c66198aefd51399498a87dbebb
SUJAL RAJPUT <sujalrajput10@gmail.com>
2025-03-26 23:21:20 +0530

    connected to database

--- src/utils/config_example.py
+++ src/utils/config_example.py
@@ -1,9 +1,13 @@
-acc_token = "your acc token"
-agenta_token = "your agent token"
+# config.example.py
+# Copy this file to `config.py` and fill in the actual values.
 
+ACC_TOKEN = "your_token_here"
 BASE_URL = "https://api.spacetraders.io/v2"
 
-HEADERS = {
-    "Authorization": f"Bearer {agenta_token}",
-    "Content-Type": "application/json",
-}
+POSTGRES_USERNAME = "your_username_here"
+POSTGRES_PASSWORD = "your_password_here"
+POSTGRES_SERVER = "your_server_here"
+POSTGRES_PORT = "5432"
+POSTGRES_DB = "your_database_here"
+
+DATABASE_URL = f"postgresql://{POSTGRES_USERNAME}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
